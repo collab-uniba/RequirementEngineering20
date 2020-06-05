359fca3bded007fab0a5af7f8042bc9e85c2b5a3
danielagir <daniela.girardi1991@gmail.com>
2020-06-05 12:45:58 +0200

    reviewers comments fixed

--- BiometricAnalysis/Discretization.R
+++ BiometricAnalysis/Discretization.R
@@ -7,6 +7,7 @@ args<-commandArgs(TRUE)
 print(args[1])
 print(args[2])
 
+
 inputFile <- args[1]
 outputFile <- args[2]
 my_data <- read_excel(inputFile)
@@ -14,9 +15,9 @@ my_data <- read_excel(inputFile)
 
 print.default(my_data)
 
-table(my_data$categoria <-discretize(as.numeric(my_data$valence), method = "cluster", breaks = 3, labels = FALSE))
-table(my_data$range <-discretize(as.numeric(my_data$valence), method = "cluster", breaks = 3, labels = NULL))
+table(my_data$categoria <-discretize(as.numeric(my_data$Valence), method = "cluster", breaks = 3, labels = FALSE))
+table(my_data$range <-discretize(as.numeric(my_data$Valence), method = "cluster", breaks = 3, labels = NULL))
 write.xlsx(my_data, file = outputFile)
-hist(my_data$valence, breaks=20, main="K-Means")
-abline(v=discretize(my_data$valence, method="cluster", categories=3, onlycuts=TRUE), col="red")
+#hist(my_data$valence, breaks=20, main="K-Means")
+#abline(v=discretize(my_data$Valence, method="cluster", categories=3, onlycuts=TRUE), col="red")
 
